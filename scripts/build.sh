ddb8e52c42eb6e48f2e0c069c4cee623cc8e048e
ariaieboy <ariaieboy.ir@gmail.com>
2023-06-12 21:51:05 +0330

    install curl and git beforehand

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,8 @@ DEBIAN_FRONTEND=noninteractive
 
 # install dependencies for build
 # source: https://learn.netdata.cloud/docs/agent/packaging/installer/methods/manual
-
+apt-get -qq update
+apt-get -y curl git
 curl -Ss 'https://raw.githubusercontent.com/netdata/netdata/master/packaging/installer/install-required-packages.sh' >/tmp/install-required-packages.sh && bash /tmp/install-required-packages.sh -i netdata-all
 
 # fetch netdata
